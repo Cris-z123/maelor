a451b64f57c2c6290b605d670ef092f4aba0e0a5
Cris-z123 <472099249@qq.com>
2026-03-05 22:05:37 +0800

    feat(T001): create directory structure and type definitions

---
+++ src/renderer/hooks/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Custom React hooks barrel export
+ */
+
+// Future hooks will be added here:
+// export { useClipboard } from './useClipboard';
+// export { useToast } from './useToast';
+// export { useInlineEdit } from './useInlineEdit';
---
+++ src/renderer/stores/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Zustand stores barrel export
+ */
+
+export { useReportStore } from './reportStore';
+// Future stores will be added here:
+// export { useOnboardingStore } from './onboardingStore';
+// export { useSettingsStore } from './settingsStore';
